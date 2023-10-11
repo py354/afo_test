f58464de48405668366db86f37006ee768e802ac
Danis Kirasirov <dbgbbu@gmail.com>
2023-10-12 01:58:45 +0300

    Add database migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from .config import settings
 
 app = Flask(__name__)
@@ -7,6 +8,7 @@ app.config.from_object(settings)
 app.config['SQLALCHEMY_DATABASE_URI'] = settings.database_url
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from . import views
 from . import models
