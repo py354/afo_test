50eea41874a8d05b4dd41905764343b46656fff8
Danis Kirasirov <dbgbbu@gmail.com>
2023-10-12 02:15:10 +0300

    Add base templates

--- app/views.py
+++ app/views.py
@@ -3,5 +3,28 @@ from app import app
 
 
 @app.route('/')
+@app.route('/index')
+@app.route('/bank-details')
 def index():
-    return render_template('index.html')
+    return render_template('bank_details.html')
+
+
+@app.route('/logout')
+def logout():
+    return 'logout'
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
+@app.route('/bank-details')
+def bank_details():
+    return render_template('bank_details.html')
+
