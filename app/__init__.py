63fc3d4ea9036fec2eaf4ee7fabbe8d4d8b899c1
Danis Kirasirov <dbgbbu@gmail.com>
2023-10-12 01:52:22 +0300

    Add .env config

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from .config import settings
 
 app = Flask(__name__)
+app.config.from_object(settings)
 
 from . import views
---
+++ app/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    SECRET_KEY: str
+
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def database_url(self):
+        r = f'postgresql://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+        print(r)
+        return r
+
+    model_config = SettingsConfigDict(env_file='.env')
+
+
+settings = Settings()
